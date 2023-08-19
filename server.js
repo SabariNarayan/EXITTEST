f81deabe4fbc7ff02c72542fae393dbb67a033cc
SabariNarayan <129525730+SabariNarayan@users.noreply.github.com>
2023-08-19 12:58:39 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Connect to your MongoDB Atlas database (replace 'your_connection_url' with your actual connection URL)
+mongoose.connect('mongodb+srv://officialsabarinarayan:9447103050@cluster0.buyzcu4.mongodb.net/exit', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB Atlas'))
+    .catch(err => console.error('Error connecting to MongoDB:', err));
+
+// Create a mongoose schema for tasks
+const taskSchema = new mongoose.Schema({
+    task: String,
+    status: String, // 'completed' or 'ongoing'
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// Create a new task
+app.post('/tasks', (req, res) => {
+    const { task, status } = req.body;
+
+    const newTask = new Task({
+        task,
+        status,
+    });
+
+    newTask.save()
+        .then(savedTask => {
+            res.status(201).json(savedTask);
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'Could not create task' });
+        });
+});
+
+// Get all tasks
+app.get('/tasks', (req, res) => {
+    Task.find()
+        .then(tasks => {
+            res.status(200).json(tasks);
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'Could not fetch tasks' });
+        });
+});
+
+app.delete('/tasks/:taskId', async (req, res) => {
+    try {
+      const { taskId } = req.params;
+  
+      // Find and remove the task by ID
+      const deletedTask = await Task.findByIdAndRemove(taskId);
+  
+      if (!deletedTask) {
+        return res.status(404).json({ error: 'Task not found' });
+      }
+  
+      res.status(204).end(); // No content, successful deletion
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Could not delete task' });
+    }
+  });
+
+  app.put('/tasks/:taskId', async (req, res) => {
+    try {
+        const { taskId } = req.params;
+        const updatedTask = req.body;
+
+        // Find and update the task by ID
+        const result = await Task.findByIdAndUpdate(taskId, updatedTask, { new: true });
+
+        if (!result) {
+            return res.status(404).json({ error: 'Task not found' });
+        }
+
+        res.status(200).json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Could not update task' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
